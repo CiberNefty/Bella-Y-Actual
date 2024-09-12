04339d3722ffd16901f6c009b0a234c4bae2a08e
CiberNefty <123023309+CiberNefty@users.noreply.github.com>
2024-09-11 21:34:51 -0500

    Update Consultas BellaYActual.sql
    Actualizamos el archivo agregando le un par de joins interesantes.

--- Base de Datos VF/Consultas BellaYActual.sql
+++ Base de Datos VF/Consultas BellaYActual.sql
@@ -61,4 +61,31 @@ SELECT * FROM Empleado;
 
 # Mostrar todas las ventas en total
 SELECT * FROM VENTA;
+#-------------------------------------------------------------------------------------------------#
+# JOINS
 
+## Mostrar todos los proveedores de cada empresa
+SELECT p.id_proveedor,p.nombre_Prove, ep.Nombre FROM Proveedor as p JOIN Empresas_Proveedoras as ep ON ep.Id_Empresas_Proveedoras = p.id_empresas_proveedoras;
+
+## Mostrar la cantidad de proveedores que tiene cada empresa
+SELECT ep.id_Empresas_proveedoras AS ID, ep.Nombre AS 'EMPRESA',COUNT(p.nombre_prove) AS 'Cantidad Proveedores' FROM Empresas_Proveedoras as ep LEFT JOIN Proveedor as p ON ep.Id_Empresas_Proveedoras = p.id_empresas_proveedoras
+group by ep.id_Empresas_proveedoras;
+
+## Mostrar el nombre del empleado, el rol que tiene y la descripcion del rol que se le otorgo.
+SELECT empleado.nombres, rol.nombre, rol.descripcion FROM empleado CROSS JOIN rol ON empleado.id_rol = rol.id_rol;
+
+## Todas las ventas que un empleado a realizado, pero solo muestrame el nombre del empleado y el identificador de la venta:
+SELECT empleado.Nombres, venta.id_venta FROM empleado INNER JOIN venta ON venta.id_empleado = empleado.id_empleado;
+
+## Mostrar las cantidades de ventas y la suma total de esas ventas de un empleado.
+SELECT empleado.Nombres Empleado, COUNT(venta.id_venta) 'Numero de ventas por empleado', SUM(venta.Total_Venta) AS 'Total de ventas por empleado' FROM empleado INNER JOIN venta ON empleado.id_empleado = venta.id_empleado GROUP BY empleado.nombres;
+
+## Muestrame las fechas de cada venta a partir de una fecha en especifico y el nombre del empleado contenga un e en su nombre:
+SELECT empleado.Nombres Empleado, venta.id_venta, venta.fecha_venta FROM empleado INNER JOIN venta ON empleado.id_empleado = venta.id_empleado
+WHERE venta.fecha_venta <= '2024-09-07' AND empleado.Nombres LIKE '%e%';
+
+## Muestrame en la tabla de pago el metodo de pago en efectivo.
+SELECT * FROM Tabla_De_Pagos WHERE Metodos_de_Pago = 'Efectivo';
+
+## Muestrame en la tabla de pago el estado de pendiente.
+SELECT * FROM Tabla_De_Pagos WHERE Estado_Pago NOT LIKE 'Paga%';
